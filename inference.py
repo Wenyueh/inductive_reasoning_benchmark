25b1fe500495e132a223f3feaccd35403dcc879c
Wenyue Hua <28013619+Wenyueh@users.noreply.github.com>
2024-12-29 14:45:52 -0800

    Update inference.py

--- inference.py
+++ inference.py
@@ -1,26 +1,32 @@
 from together import Together
-import argparse, os, random, config
+import argparse, os, random, config, json
 from tqdm import tqdm
 from synthetic_data_generation import synthetic_data_parser, generate_rules, generate_data, apply_rule
 
-
 def call_model(args, text):
-    if 'llama' in args.model:
-        client = Together(api_key=os.environ['TOGETHER_AI_API'])
-        output = client.chat.completions.create(
-            model=args.model,
-            messages=[{"role": "user", "content": text}],
-            stream=False,
-            max_tokens=2000,
-        )
-        return output.choices[0].message.content
+    client = Together(api_key=os.environ['TOGETHER_AI_API'])
+    output = client.chat.completions.create(
+        model=args.model,
+        messages=[{"role": "user", "content": text}],
+        stream=False,
+        max_tokens=2000,
+    )
+    return output.choices[0].message.content
     
 def extract_answer(output):
     starts = [i for i in range(len(output)) if output.startswith('<START>', i)]
     ends = [i for i in range(len(output)) if output.startswith('<END>', i)]
-    last_start = starts[-1]
-    last_end = ends[-1]
-    output = output[last_start:last_end]
+    if len(starts) == 0 or len(ends) == 0:
+        return {'':''}
+    if len(starts) == len(ends):
+        last_start = starts[-1]
+        last_end = ends[-1]
+    else:
+        early = min(len(starts), len(ends))
+        last_start = starts[early-1]
+        last_end = ends[early-1]
+
+    output = output[last_start:last_end].replace('\n\n', '\n')
 
     rules = {}
     lines = output.split('\n')
@@ -28,6 +34,10 @@ def extract_answer(output):
         if '->' in line:
             rule = line.split('->')
             rules[rule[0].strip()] = rule[1].strip()
+
+    if rules == {}:
+        return {'':''}
+    
     return rules
 
 def evaluation_single_datapoint(args, data, ground_truth_rules, predicted_rules):
@@ -60,10 +70,12 @@ def evaluation_single_datapoint(args, data, ground_truth_rules, predicted_rules)
     return recall, precision, compatibility
 
 def run_inference_and_evaluation(args, datapoints, rules):
+    tosave = []
     average_recall = []
     average_precision = []
     average_compatibility = []
     for i, datapoint in enumerate(tqdm(datapoints)):
+        one_data_to_save = {}
         print(rules[i])
         print('------------------------------------')
         output = call_model(args, datapoint[1])
@@ -77,15 +89,34 @@ def run_inference_and_evaluation(args, datapoints, rules):
         average_recall.append(recall)
         average_precision.append(precision)
         average_compatibility.append(compatibility)
+        one_data_to_save['sample_data'] = datapoint[0]
+        one_data_to_save['input_prompt'] = datapoint[1]
+        one_data_to_save['ground_truth_rules'] = rules[i]
+        one_data_to_save['output'] = output
+        one_data_to_save['predicted_rules'] = answer
+        one_data_to_save['recall'] = recall
+        one_data_to_save['precision'] = precision
+        one_data_to_save['compatibility'] = compatibility
 
-    print(f'Average Recall: {sum(average_recall)/len(average_recall)}')
-    print(f'Average Precision: {sum(average_precision)/len(average_precision)}')
-    print(f'Average Compatibility: {sum(average_compatibility)/len(average_compatibility)}')
+        tosave.append(one_data_to_save)
+
+    average_recall = sum(average_recall)/len(average_recall)
+    average_precision = sum(average_precision)/len(average_precision)
+    average_compatibility = sum(average_compatibility)/len(average_compatibility)
+
+    print(f'Average Recall: {average_recall}')
+    print(f'Average Precision: {average_precision}')
+    print(f'Average Compatibility: {average_compatibility}')
+
+    tosave = {'original_data': tosave, 'average_recall': average_recall, 'average_precision': average_precision, 'average_compatibility': average_compatibility}
+
+    with open(args.save_directory, 'w') as f:
+        json.dump(tosave, f)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     # parameters for experiment setting
-    parser.add_argument('--model', type=str, default="meta-llama/Meta-Llama-3.1-405B-Instruct-Turbo")
+    parser.add_argument('--model', type=str, default="Qwen QwQ-32B-Preview")
     parser.add_argument('--num_of_datapoints', type=int, default=10)
 
     # parameters for synthetic data generation
@@ -95,6 +126,12 @@ if __name__ == '__main__':
     parser.add_argument('--number_of_rules', type=int, default=2, help='number of rules, must be smaller than vocab_size^k')
     parser.add_argument('--sample_size_times', type=int, default=4, help='number of sample to compute induction on, 1 means it is just the characteristic sample')
     args = parser.parse_args()
+
+    model_name = args.model.replace('/', '_')
+
+    save_directory = f'result/{model_name}_{args.type}_{args.k}_{args.vocab_size}_{args.number_of_rules}_{args.sample_size_times}.json'
+    args.save_directory = save_directory
+
     random.seed(0)
 
     # Generate vocab
