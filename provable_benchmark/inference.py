f4eca042d46f665e20696aadc2958a68577b7143
Wenyue Hua <28013619+Wenyueh@users.noreply.github.com>
2025-02-14 17:01:36 -0500

    Update inference.py

--- provable_benchmark/inference.py
+++ provable_benchmark/inference.py
@@ -1,10 +1,9 @@
 import argparse, os, random, config, json, sys, time
 from tqdm import tqdm
 from synthetic_data_generation import synthetic_data_parser, generate_rules, generate_data, apply_rule
-from utils import extract_answer
-from model import call_model
 sys.path.add('..')
-
+from model import call_model
+from utils import extract_answer
 
 def evaluation_single_datapoint(args, data, ground_truth_rules, predicted_rules):
     # for i,o in ground_truth_rules.items():
