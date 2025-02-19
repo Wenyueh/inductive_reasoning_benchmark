6699c1139be0f8e3910e6866f08f95b0af5e6b29
Wenyue Hua <28013619+Wenyueh@users.noreply.github.com>
2025-02-19 14:59:31 -0500

    Update provable_run.py

--- provable_benchmark/provable_run.py
+++ provable_benchmark/provable_run.py
@@ -1,10 +1,8 @@
 import argparse, os, random, config, json, sys, time
 from tqdm import tqdm
 from synthetic_data_generation import synthetic_data_parser, generate_rules, generate_data, apply_rule
-
-sys.path.add('..')
-from model import call_model
 from utils import extract_answer
+from model import call_model
 
 
 def evaluation_single_datapoint(args, data, ground_truth_rules, predicted_rules):
@@ -41,17 +39,16 @@ def evaluation_single_datapoint(args, data, ground_truth_rules, predicted_rules)
 
     return recall, precision, compatibility
 
-def run_inference_and_evaluation(args, datapoints, rules):
+def run_inference_and_evaluation(args, datapoints, rules, existent_save_directory):
     tosave = []
     average_recall = []
     average_precision = []
     average_compatibility = []
-    for i, datapoint in enumerate(tqdm(datapoints)):
+    for i, datapoint in enumerate(tqdm(datapoints, desc='Running inference and evaluation')):
         one_data_to_save = {}
         output = call_model(args, datapoint[1])
         answer = extract_answer(output)
         recall, precision, compatibility = evaluation_single_datapoint(args, datapoint[0], rules[i], answer)
-        print(f'Recall: {recall}, Precision: {precision}, Compatibility: {compatibility}')
         average_recall.append(recall)
         average_precision.append(precision)
         average_compatibility.append(compatibility)
@@ -70,7 +67,7 @@ def run_inference_and_evaluation(args, datapoints, rules):
     average_precision = sum(average_precision)/len(average_precision)
     average_compatibility = sum(average_compatibility)/len(average_compatibility)
 
-    print('Print Current Congifuration:')
+    print('Print Current Configuration:')
     print(f'function type: {args.type}, k: {args.k}, vocab_size: {args.vocab_size}, number_of_rules: {args.number_of_rules}')
     print(f'Average Recall: {average_recall}')
     print(f'Average Precision: {average_precision}')
@@ -79,14 +76,17 @@ def run_inference_and_evaluation(args, datapoints, rules):
     tosave = {'original_data': tosave, 'average_recall': average_recall, 'average_precision': average_precision, 'average_compatibility': average_compatibility, 'k': args.k, 'vocab_size': args.vocab_size, 'number_of_rules': args.number_of_rules, 'type': args.type}
 
     with open(args.save_directory, 'a') as f:
-        json.dump(tosave, f)
+        json.dump(tosave, f, indent=4)
+
+    with open(existent_save_directory, 'w') as f:
+        json.dump(tosave, f, indent=4)
 
     return average_recall, average_precision, average_compatibility
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     # parameters for experiment setting
-    parser.add_argument('--model', type=str, default="Qwen/QwQ-32B-Preview")
+    parser.add_argument('--model', type=str, default="meta-llama/Llama-3.3-70B-Instruct-Turbo")
     parser.add_argument('--num_of_datapoints', type=int, default=10)
 
     # parameters for synthetic data generation
@@ -101,9 +101,14 @@ if __name__ == '__main__':
 
     model_name = args.model.replace('/', '_')
 
-    save_directory = f'standard_benchmark_result/{model_name}.json'
+    args.save_directory = f'standard_benchmark/{model_name}.json'
+    if not os.path.exists('standard_benchmark'):
+       os.mkdir('standard_benchmark')
 
-    with open(save_directory, mode='w') as f:
+    print('Start running standard benchmark')
+    print("Results will be saved in ", args.save_directory)
+
+    with open(args.save_directory, mode='w') as f:
         json.dump([], f)
     
     random.seed(0)
@@ -119,19 +124,40 @@ if __name__ == '__main__':
         total_weight = 0
         scores = []
         for k in range(2, args.k+1): # k = 2, 3, 4
+            args.k = k
             for vocab_size in range(5, args.vocab_size+1): # vocab_size = 5, 6, 7, 8
-                for number_of_rules in range(3, args.number_of_rules+1): # number_of_rules = 3, 4, 5
-                    number_of_rules = min(number_of_rules, vocab_size**k)
-                    args.k = k
-                    args.vocab_size = vocab_size
+                args.vocab_size = vocab_size
+                for number_of_rules in range(3, args.number_of_rules+1): # number_of_rules = 3, 4, 5             
                     args.number_of_rules = number_of_rules
-
+                    print(f'Running the setting of function type {type}, k: {k}, vocab_size: {vocab_size}, number_of_rules: {number_of_rules}')
                     weight = vocab_size**k * number_of_rules
 
-                    rules = generate_rules(args)
-                    datapoints = generate_data(args, rules)
-
-                    average_recall, average_precision, average_compatibility = run_inference_and_evaluation(args, datapoints, rules)
+                    model_name = args.model.replace('/', '_')
+                    existent_save_directory = f'result/{args.type}/{model_name}_{args.type}_{args.k}_{args.vocab_size}_{args.number_of_rules}_{args.sample_size_times}.json'
+                    if not os.path.exists(f'result/{args.type}'):
+                        os.mkdir(f'result/{args.type}')
+                    if os.path.exists(existent_save_directory):
+                        print('This setting has been done before: ', existent_save_directory)
+                        with open(existent_save_directory, 'r') as f:
+                            data = json.load(f)
+                        average_recall = data['average_recall']
+                        average_precision = data['average_precision']
+                        average_compatibility = data['average_compatibility']
+                        data['k'] = args.k 
+                        data['vocab_size'] = args.vocab_size
+                        data['number_of_rules'] = args.number_of_rules
+                        data['type'] = args.type
+                        with open(args.save_directory, 'a') as f:
+                            json.dump(data, f)
+                    else:
+                        number_of_rules = min(number_of_rules, vocab_size**k)
+                        args.k = k
+                        args.vocab_size = vocab_size
+                        args.number_of_rules = number_of_rules
+                        rules = generate_rules(args)
+                        datapoints = generate_data(args, rules)
+                        average_recall, average_precision, average_compatibility = run_inference_and_evaluation(args, datapoints, rules, existent_save_directory)
+                    
                     scores.append((average_recall, average_precision, average_compatibility, weight))
 
                     total_weight += weight
@@ -146,16 +172,18 @@ if __name__ == '__main__':
         print(f'Weighted {type} Recall: {weighted_recall}')
         print(f'Weighted {type} Precision: {weighted_precision}')
         print(f'Weighted {type} Compatibility: {weighted_compatibility}')
-        with open(save_directory, 'a') as f:
+        with open(args.save_directory, 'a') as f:
             json.dump({f'{type}_average_recall': recall, f'{type}_average_precision': precision, f'{type}_average_compatibility': compatibility}, f)
 
         recall += weighted_recall
         precision += weighted_precision
         compatibility += weighted_compatibility
 
-    print(f'Average Recall: {recall}')
-    print(f'Average Precision: {precision}')
-    print(f'Average Compatibility: {compatibility}')
+    print(f'Average Recall: {recall/3}')
+    print(f'Average Precision: {precision/3}')
+    print(f'Average Compatibility: {compatibility/3}')
 
-    with open(save_directory, 'a') as f:
+    with open(args.save_directory, 'a') as f:
         json.dump({'average_recall': recall, 'average_precision': precision, 'average_compatibility': compatibility}, f)
+
+        
