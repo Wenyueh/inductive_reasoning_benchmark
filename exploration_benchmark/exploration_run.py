f78e6f12dacf9410f5c52e4e704ae17939e2acf0
Wenyue Hua <28013619+Wenyueh@users.noreply.github.com>
2025-02-19 16:42:31 -0500

    Update exploration_run.py

--- exploration_benchmark/exploration_run.py
+++ exploration_benchmark/exploration_run.py
@@ -28,7 +28,7 @@ def evaluation_single_datapoint(args, data, ground_truth_rules, predicted_rules)
 
     return compatibility, is_minimum_description
 
-def run_inference_and_evaluation(args, datapoints, rules):
+def run_inference_and_evaluation(args, datapoints, rules, existent_save_directory):
     tosave = []
     average_compatibility = []
     average_length = []
@@ -37,7 +37,6 @@ def run_inference_and_evaluation(args, datapoints, rules):
         output = call_model(args, datapoint[1])
         answer = extract_answer(output)
         compatibility, is_minimum_description = evaluation_single_datapoint(args, datapoint[0], rules[i], answer)
-        print(f'Compatibility: {compatibility}, Is_minimum_description: {is_minimum_description}')
         average_compatibility.append(compatibility)
         if is_minimum_description is not None:
             average_length.append(is_minimum_description)
@@ -63,6 +62,9 @@ def run_inference_and_evaluation(args, datapoints, rules):
 
     tosave = {'original_data': tosave, 'average_is_minimum_description': average_is_minimum_description, 'average_compatibility': average_compatibility, 'k': args.k, 'vocab_size': args.vocab_size, 'number_of_rules': args.number_of_rules, 'type': args.type}
 
+    with open(existent_save_directory, 'w') as f:
+        json.dump(tosave, f, indent=4)
+
     with open(args.save_directory, 'a') as f:
         json.dump(tosave, f)
 
@@ -70,24 +72,28 @@ def run_inference_and_evaluation(args, datapoints, rules):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    # parameters for experiment setting
-    parser.add_argument('--model', type=str, default="Qwen/QwQ-32B-Preview")
+    # parameters for synthetic data generation
+    parser.add_argument('--type', type=str, default='IOSL', help='only IOSL is supported')
+    parser.add_argument('--k', type=int, default=4, help='number of k in ISL_k or OSL_k')
+    parser.add_argument('--vocab_size', type=int, default=8, help='number of vocab')
+    parser.add_argument('--number_of_rules', type=int, default=5, help='number of rules, must be smaller than vocab_size^k')
+    parser.add_argument('--sample_size', type=int, default=100, help='number of sample to compute induction on')
+    parser.add_argument('--shot_number', type=int, default=1)
     parser.add_argument('--num_of_datapoints', type=int, default=10)
 
-    # parameters for synthetic data generation
-    parser.add_argument('--k', type=int, default=4, help='number of k in IOSL')
-    parser.add_argument('--vocab_size', type=int, default=8, help='number of vocab')
-    parser.add_argument('--number_of_rules', type=int, default=4, help='number of rules, must be smaller than vocab_size^k')
-    parser.add_argument('--sample_size', type=int, default=100, help='number of sample to compute induction on, 1 means it is just the characteristic sample')
+    # parameters for experiment setting
+    parser.add_argument('--model', type=str, default="Qwen/QwQ-32B-Preview")
     parser.add_argument('--reevaluate', action='store_true', help='re-evaluate the result')
 
     args = parser.parse_args()
 
     model_name = args.model.replace('/', '_')
 
-    save_directory = f'result/{model_name}.json'
+    args.save_directory = f'exploration_result/{model_name}.json'
+    if not os.path.exists('exploration_result'):
+        os.makedirs('exploration_result')
 
-    with open(save_directory, mode='w') as f:
+    with open(args.save_directory, mode='w') as f:
         json.dump([], f)
     
     random.seed(0)
@@ -100,22 +106,40 @@ if __name__ == '__main__':
     compatibility = 0
     total_weight = 0
     scores = []
+    assert args.k > 2, 'k must be larger than 2'
+    assert args.vocab_size > 5, 'vocab_size must be larger than 5'
+    assert args.number_of_rules > 3, 'number_of_rules must be larger than 3'
     for k in range(2, args.k+1): # k = 2, 3, 4
         for vocab_size in range(5, args.vocab_size+1): # vocab_size = 5, 6, 7, 8
             for number_of_rules in range(3, args.number_of_rules+1): # number_of_rules = 3, 4, 5
-                number_of_rules = min(number_of_rules, vocab_size**k)
-                args.k = k
-                args.vocab_size = vocab_size
-                args.number_of_rules = number_of_rules
 
-                args.sample_size = args.vocab_size**k * 2
+                # save each individual setting result
+                sample_size = min(args.sample_size, vocab_size**k * 2)
+                model_name = args.model.replace('/', '_')
+                existent_save_directory = f'exploration_result/{model_name}_{k}_{vocab_size}_{number_of_rules}_{sample_size}.json'
+                if not os.path.exists('exploration_result'):
+                    os.mkdir('exploration_result')
 
-                weight = vocab_size**k * number_of_rules
+                print(f"Start running benchmarking with k={k}, vocab_size={vocab_size}, number_of_rules={number_of_rules}")
+                if os.path.exists(existent_save_directory):
+                    with open(existent_save_directory, 'r') as f:
+                        data = json.load(f)
+                    average_is_minimum_description = data['average_is_minimum_description']
+                    average_compatibility = data['average_compatibility']
+                else:
+                    number_of_rules = min(number_of_rules, vocab_size**k)
+                    args.k = k
+                    args.vocab_size = vocab_size
+                    args.number_of_rules = number_of_rules
 
-                rules = generate_rules(args)
-                datapoints = generate_data(args, rules)
+                    args.sample_size = args.vocab_size**k * 2
 
-                average_compatibility, average_is_minimum_description = run_inference_and_evaluation(args, datapoints, rules)
+                    weight = vocab_size**k * number_of_rules
+
+                    rules = generate_rules(args)
+                    datapoints = generate_data(args, rules)
+
+                    average_compatibility, average_is_minimum_description = run_inference_and_evaluation(args, datapoints, rules, existent_save_directory)
                 scores.append((average_compatibility, average_is_minimum_description, weight))
 
                 total_weight += weight
@@ -129,5 +153,5 @@ if __name__ == '__main__':
     print(f'Weighted IOSL Compatibility: {weighted_compatibility}')
     print(f'Weighted IOSL Is_minimum_description: {weighted_is_minimum_description}')
 
-    with open(save_directory, 'a') as f:
+    with open(args.save_directory, 'a') as f:
         json.dump({f'{type}_average_recall': recall, f'{type}_average_precision': precision, f'{type}_average_compatibility': compatibility}, f)
