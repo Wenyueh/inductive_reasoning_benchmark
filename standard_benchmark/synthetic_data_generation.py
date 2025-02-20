a3aba851394e003699f5f1335a4f8fad535fbeaa
Wenyue Hua <28013619+Wenyueh@users.noreply.github.com>
2025-02-19 23:42:52 -0500

    Update synthetic_data_generation.py

--- standard_benchmark/synthetic_data_generation.py
+++ standard_benchmark/synthetic_data_generation.py
@@ -4,7 +4,7 @@ import time
 import config
 from tqdm import tqdm
 import sys
-sys.path.add('..')
+sys.path.append('..')
 from utils import generate_all_k_strings, translate_input_output_pairs, translate_fewshot_input_output_pairs
 
 # ISL function: input-strictly-local-k function
